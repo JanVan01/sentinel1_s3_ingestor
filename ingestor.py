f431d1e2f48e31da9b69f37b1b99c174cde16188
JanVan01 <j_vanz01@uni-muenster.de>
2017-06-07 16:37:19 +0200

    add upload to s3 functionality

--- ingestor.py
+++ ingestor.py
@@ -6,9 +6,9 @@ from upload.base import BaseUploader
 from upload.s3 import S3Uploader
 import credentials
 
-class Ingestor:
 
-    def __init__(self, search: BaseSearch, downloader:BaseDownloader, uploader:BaseUploader):
+class Ingestor:
+    def __init__(self, search: BaseSearch, downloader: BaseDownloader, uploader: BaseUploader):
         self.search = search
         self.downloader = downloader
         self.uploader = uploader
@@ -16,15 +16,12 @@ class Ingestor:
     def ingest(self):
         result = self.search.search()
         path = self.downloader.download(result)
-        self.uploader.upload(path)
-
+        self.uploader.upload(path, "path/on/server")
 
 
 if __name__ == '__main__':
-    search = CopernicusSearch(credentials.copernicus_hub['username'],credentials.copernicus_hub['password'])
+    search = CopernicusSearch(credentials.copernicus_hub['username'], credentials.copernicus_hub['password'])
     downloader = CopernicusDownloader()
-    uploader = S3Uploader()
+    uploader = S3Uploader(credentials.s3_bucket['name'])
     ingestor = Ingestor(search, downloader, uploader)
     ingestor.ingest()
-
-
--- upload/base.py
+++ upload/base.py
@@ -3,8 +3,8 @@ import string
 
 class BaseUploader:
 
-    def upload(self, path:string):
+    def upload(self, local_file_path:string, upload_path:string):
         raise NotImplementedError('This method is not implemented in the base class')
 
-    def exists(self, name):
+    def exists(self, path):
         raise NotImplementedError('This method is not implemented in the base class')
