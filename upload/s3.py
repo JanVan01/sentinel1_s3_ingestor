d25cfad80289136ef8361fcf57170d43aef0859c
JanVan01 <j_vanz01@uni-muenster.de>
2017-07-02 15:47:45 +0200

    remove the need for aws credentials

--- upload/s3.py
+++ upload/s3.py
@@ -6,31 +6,18 @@ import sys
 
 from boto3.s3.transfer import S3Transfer
 
-import credentials
-
 from upload.base import BaseUploader
 
-AWS_ACCESS_KEY_ID = credentials.aws['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = credentials.aws['AWS_SECRET_ACCESS_KEY']
-
-
 class S3Uploader(BaseUploader):
-
     def __init__(self, bucket_name):
         self.bucket_name = bucket_name
-        self.client = boto3.client(
-            's3',
-            region_name='eu-central-1',
-            config=boto3.session.Config(signature_version='s3v4'),
-            # Hard coded strings as credentials, not recommended.
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        self.client = boto3.client('s3')
 
     def upload(self, local_file_path, upload_path):
         print("uploading file %s to bucket %s" % (local_file_path, self.bucket_name))
         transfer = S3Transfer(self.client)
         transfer.upload_file(filename=local_file_path, bucket=self.bucket_name, key=upload_path,
-                                callback=ProgressPercentage(local_file_path))
+                             callback=ProgressPercentage(local_file_path))
 
     def exists(self, path):
         response = self.client.list_objects(Bucket=self.bucket_name, Prefix=path)
