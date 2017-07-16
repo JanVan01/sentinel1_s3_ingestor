ac4fbf6b19581b7899748f6c95d1fcbadae86747
JanVan01 <j_vanz01@uni-muenster.de>
2017-07-16 19:51:50 +0200

    add a config flag to disable shutting down after ingestion

--- ingestor.py
+++ ingestor.py
@@ -1,7 +1,8 @@
+from base64 import b64decode
+
 from api.copernicus import CopernicusAPI
 from upload.s3 import S3Uploader
 import credentials
-import boto3
 import boto3.session
 
 class Ingestor:
@@ -17,16 +18,16 @@ class Ingestor:
                 download_path = self.__api.download(uuid)
                 self.__uploader.upload(download_path, details['filename'])
                 self.__api.remove(download_path)
-	print('Shutting down instance')
-	#conn = boto.ec2.connect_to_region("eu-central-1") # or your region
-	ec2 = boto3.resource('ec2', region_name="eu-central-1")
-	# Get the current instance's id
-	#my_id = boto.utils.get_instance_metadata()['instance-id']
-	#conn.stop_instances(instance_ids=[my_id])
-	ec2.instances.filter(InstanceIds=[credentials.ec2_instance['id']]).stop()
 
 if __name__ == '__main__':
     uploader = S3Uploader(credentials.s3_bucket['name'])
     api = CopernicusAPI(credentials.copernicus_hub['username'], credentials.copernicus_hub['password'])
     ingestor = Ingestor(api, uploader)
     ingestor.ingest()
+
+    ec2 = boto3.resource('ec2', region_name="eu-central-1")
+    instance = ec2.Instance(credentials.ec2_instance['id'])
+    user_data = b64decode(instance.describe_attribute(Attribute='userData')['UserData']['Value']).decode('utf-8')
+    if user_data != 'dev':
+        print('Shutting down instance')
+        instance.stop()
